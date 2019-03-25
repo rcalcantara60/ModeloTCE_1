4e561795939db76372f19be38ab4865b55bf4712
Rodrigo Costa <rodrial_7@hotmail.com>
2019-03-25 17:31:57 -0300

    A

--- CR_Api/ApiService/Startup.cs
+++ CR_Api/ApiService/Startup.cs
@@ -34,7 +34,7 @@ namespace ApiService
         private Container _container { get; set; }
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             _container = InjectorContainer.GetContainer();            
